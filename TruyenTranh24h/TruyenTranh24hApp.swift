0b910f406e4df4a615fe3f6b9ebbfa9c712525b3
Phu Huynh <phu@filesoft.tech>
2021-04-13 13:47:38 +0700

    Initial Commit

---
+++ TruyenTranh24h/TruyenTranh24hApp.swift
@@ -0,0 +1,17 @@
+//
+//  TruyenTranh24hApp.swift
+//  TruyenTranh24h
+//
+//  Created by Huynh Tan Phu on 13/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct TruyenTranh24hApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
