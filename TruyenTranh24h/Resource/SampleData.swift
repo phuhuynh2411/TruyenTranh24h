843d5f8b5509e1531cfa97e12b59a5b47ed83ad1
Phu Huynh <phu@filesoft.tech>
2021-04-14 10:36:12 +0700

    + Add unit test for loading stories.json + Add View folder

--- TruyenTranh24h/Resource/SampleData.swift
+++ TruyenTranh24h/Resource/SampleData.swift
@@ -8,10 +8,13 @@
 import Foundation
 
 struct SampleData {
+    
+    static var storyFilename = "stories"
+    
     static func stories() -> [Story] {
         var stories = [Story]()
 
-        if let url = Bundle.main.url(forResource: "stories", withExtension: ".json") {
+        if let url = Bundle.main.url(forResource: storyFilename, withExtension: ".json") {
             // load stories json file
             let jsonDecoder = JSONDecoder()
             jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
@@ -22,7 +25,7 @@ struct SampleData {
                 print(error.localizedDescription)
             }
         } else {
-            print("Could not find the resource file.")
+            print("Could not find the resource file.\(storyFilename)")
         }
        
         return stories
--- TruyenTranh24hTests/SampleDataTest.swift
+++ TruyenTranh24hTests/SampleDataTest.swift
@@ -1,5 +1,5 @@
 //
-//  TruyenTranh24hTests.swift
+//  SampleData.swift
 //  TruyenTranh24hTests
 //
 //  Created by Huynh Tan Phu on 13/04/2021.
@@ -8,7 +8,7 @@
 import XCTest
 @testable import TruyenTranh24h
 
-class TruyenTranh24hTests: XCTestCase {
+class SampleDataTest: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -18,16 +18,24 @@ class TruyenTranh24hTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
+    }
+    
+    func testLoadStories() {
+        // Load the stories successfuly
+        SampleData.storyFilename = "stories"
+        let stories = SampleData.stories()
+        XCTAssertGreaterThan(stories.count, 0)
+        
+        // Cannot load stories from json file
+        SampleData.storyFilename = "invalid file name"
+        let stories1 = SampleData.stories()
+        XCTAssertEqual(stories1.count, 0)
+    
     }
 
 }
