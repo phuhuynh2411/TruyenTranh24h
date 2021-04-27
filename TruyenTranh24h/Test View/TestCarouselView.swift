326d6d7a7bf16891fe38b563c0f03f78c7c02b63
Phu Huynh <phu@filesoft.tech>
2021-04-27 11:07:03 +0700

    + Add TestCarouselView file

---
+++ TruyenTranh24h/Test View/TestCarouselView.swift
@@ -0,0 +1,20 @@
+//
+//  TestCarouselView.swift
+//  TruyenTranh24h
+//
+//  Created by Huynh Tan Phu on 27/04/2021.
+//
+
+import SwiftUI
+
+struct TestCarouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TestCarouselView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestCarouselView()
+    }
+}
